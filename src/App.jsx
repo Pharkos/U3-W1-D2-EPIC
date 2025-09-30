5900ccc3957a46de55f6b5504f1ca13be4e4c14c
Pharkos <vakhopharkosadze1@gmail.com>
2025-09-30 16:13:32 +0200

    Add Cards From Books Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { Col, Container, Row } from "react-bootstrap";
 import "./App.css";
+import AllTheBooks from "./components/AllTheBooks";
 import TopBar from "./components/TopBar";
 
 function App() {
   return (
     <>
       <TopBar brandName="Book-World" />
-      <Container>
-        <Row>
-          <Col>1 of 1</Col>
-        </Row>
-      </Container>
+      <AllTheBooks />
     </>
   );
 }
